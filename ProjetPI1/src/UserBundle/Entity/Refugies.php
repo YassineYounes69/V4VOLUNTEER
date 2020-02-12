4fd4f7a6a9ec7ec642f6d4c652c0d2637205f962
afamine <amine.farah@esprit.tn>
2020-02-12 09:03:31 +0100

    aminee

---
+++ ProjetPI1/src/UserBundle/Entity/Refugies.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace ProjectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Refugies
+ *
+ * @ORM\Table(name="refugies")
+ * @ORM\Entity(repositoryClass="ProjectBundle\Repository\RefugiesRepository")
+ */
+class Refugies
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_ref", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id_ref;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pays_ref", type="string", length=255)
+     */
+    private $paysRef;
+
+    /**
+     * @return int
+     */
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_membre", referencedColumnName="id", nullable=true)
+     * })
+     */
+    private $id_membre;
+
+    /**
+     * @return mixed
+     */
+    public function getIdMembre()
+    {
+        return $this->id_membre;
+    }
+
+    /**
+     * @param mixed $id_membre
+     */
+    public function setIdMembre($id_membre)
+    {
+        $this->id_membre = $id_membre;
+    }
+
+    /**
+     * @param int $ageRef
+     */
+    public function setAgeRef($ageRef)
+    {
+        $this->ageRef = $ageRef;
+    }
+
+    /**
+     * @param string $etatRef
+     */
+    public function setEtatRef($etatRef)
+    {
+        $this->etatRef = $etatRef;
+    }
+
+
+    public function getAgeRef()
+    {
+        return $this->ageRef;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdRef()
+    {
+        return $this->id_ref;
+    }
+
+    /**
+     * @param int $id_ref
+     */
+    public function setIdRef($id_ref)
+    {
+        $this->id_ref = $id_ref;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaysRef()
+    {
+        return $this->paysRef;
+    }
+
+    /**
+     * @param string $paysRef
+     */
+    public function setPaysRef($paysRef)
+    {
+        $this->paysRef = $paysRef;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomRef()
+    {
+        return $this->nomRef;
+    }
+
+    /**
+     * @param string $nomRef
+     */
+    public function setNomRef($nomRef)
+    {
+        $this->nomRef = $nomRef;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenomRef()
+    {
+        return $this->prenomRef;
+    }
+
+    /**
+     * @param string $prenomRef
+     */
+    public function setPrenomRef($prenomRef)
+    {
+        $this->prenomRef = $prenomRef;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGenderRef()
+    {
+        return $this->genderRef;
+    }
+
+    /**
+     * @param string $genderRef
+     */
+    public function setGenderRef($genderRef)
+    {
+        $this->genderRef = $genderRef;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEtatRef()
+    {
+        return $this->etatRef;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="etat_ref", type="string", length=255)
+     */
+    private $etatRef;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_ref", type="string", length=255)
+     */
+    private $nomRef;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom_ref", type="string", length=255)
+     */
+    private $prenomRef;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age_ref", type="integer")
+     */
+    private $ageRef;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="gender_ref", type="string", length=255)
+     */
+    private $genderRef;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255,nullable=true)
+     */
+    private $image;
+
+
+
+}
+
